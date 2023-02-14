3cc0acde1d96e3963e0ffece42b3b2fc6f6b7a84
RK-404 <rizwan_k@RIZWANs-MacBook-Air.local>
2023-02-13 22:40:39 -0500

    Solved it

--- index.js
+++ index.js
@@ -1,24 +1,39 @@
 // Should convert a string representing a number into a number.
 // If the number is not an integer, round down to the nearest integer.
 // If the string cannot be parsed as a number, return NaN.
-function convertStringToInteger(string) {}
+function convertStringToInteger(string) {
+  return Number.parseInt(string);
+}
 
 // Should convert a string representing a number into a number.
 // Should work with integers or numbers with decimals.
 // If the string cannot be parsed as a number, return a string 
 // that says "Not a number.".
-function convertStringToNumber(string) {}
+function convertStringToNumber(string) {
+  let result = Number.parseFloat(string);
+  return Number.isNaN(result) ? "Not a number." : result;
+}
 
 // Should round a number to an integer. The instruction will be
 // one of the following strings:
 // "up" -- round up to the next highest integer
 // "down" -- round down to the next lowest integer
 // If no instruction is given, round to the nearest integer.
-function roundToInteger(number, instruction) {}
+function roundToInteger(number, instruction) {
+  if (instruction === "up") {
+    return Math.ceil(number);
+  }
+  if (instruction === "down") {
+    return Math.floor(number);
+  }
+  return Math.round(number);
+}
 
 // Returns true if the given argument is an array, or
 // false if it's not.
-function isItAnArray(item) {}
+function isItAnArray(item) {
+  return Array.isArray(item);
+}
 
 // The next few functions use an object with the following shape:
 const countriesObject = {
@@ -30,10 +45,14 @@ const countriesObject = {
 }
 
 // Should return an array of the given country names.
-function returnCountryNames(object) {}
+function returnCountryNames(object) {
+  return Object.keys(object);
+}
 
 // Should return an array of the given country capital cities.
-function returnCapitalCities(object) {}
+function returnCapitalCities(object) {
+  return Object.values(object);
+}
 
 // Should return an array of arrays, each containing a
 // country's name, and then that country's capital.
@@ -47,7 +66,9 @@ function returnCapitalCities(object) {}
   [ 'Pakistan', 'Islamabad' ]
 ]
 */
-function returnCountriesAndCapitals(object) {}
+function returnCountriesAndCapitals(object) {
+  return Object.entries(object);
+}
 
 // Don't change anything below this line.
 module.exports = {
